3614b2c39693a43980d0646465165ab2c9ebb22b
buentellopc <buentellopc@com>
2022-08-20 11:37:57 -0500

    [step47] join tables

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Employee.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Employee.java
@@ -10,7 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Employee {
 
     @Id
